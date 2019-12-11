53e8f90fcad30d00ab8c224ea98c4b3111b2a6a5
Trong Garson <tronggarson@gmail.com>
2019-12-11 13:38:16 -0500

    add location api

---
+++ php/location/mine.php
@@ -0,0 +1,37 @@
+<?php
+
+error_reporting(0);
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+
+include_once '../config/database.php';
+include_once '../model/location.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+session_start();
+
+if(empty($_SESSION)) {
+  http_response_code(401);
+  echo "No Active Session";
+  exit;
+}
+
+$userId = $_SESSION["userId"];
+if(empty($userId)) {
+  http_response_code(401);
+  echo "Not Authenticated";
+  exit;
+}
+
+$location = new Location($db);
+
+$locations = $location->findAllFor($userId);
+
+http_response_code(200);
+echo json_encode($locations);
+
+?>
