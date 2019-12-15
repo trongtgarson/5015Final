c5300cc1d4f4a0ee78f01ac089292adb7adc9a73
Trong Garson <tronggarson@gmail.com>
2019-12-15 09:29:49 -0500

    update sql to match version used on server and correct schema name

--- sql/create_initial_schema.sql
+++ sql/create_initial_schema.sql
@@ -1,26 +1,26 @@
-CREATE SCHEMA 'wimc' ;
+use FA10_5015_tua20258;
 
-CREATE TABLE `wimc`.`users` (
+CREATE TABLE `users` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(255) NOT NULL,
   `password` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE);
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC));
 
-CREATE TABLE `wimc`.`locations` (
+CREATE TABLE `locations` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `latitude` DECIMAL(10,8) NOT NULL,
   `longitude` DECIMAL(11,8) NOT NULL,
   `date` DATETIME NULL,
   PRIMARY KEY (`id`));
 
-ALTER TABLE `wimc`.`locations` 
+ALTER TABLE `locations`
 ADD COLUMN `userId` INT NOT NULL AFTER `date`,
-ADD INDEX `userId_idx` (`userId` ASC) VISIBLE;
+ADD INDEX `userId_idx` (`userId` ASC);
 ;
-ALTER TABLE `wimc`.`locations` 
+ALTER TABLE `locations`
 ADD CONSTRAINT `locationUserId`
 FOREIGN KEY (`userId`)
-REFERENCES `wimc`.`users` (`id`)
+REFERENCES `users` (`id`)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
